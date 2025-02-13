4b9488fff2cc604aacaea05a4408aaa649ff9695
AsifShawon <asifbhshawon71@gmail.com>
2025-02-13 12:57:10 +0600

    sus

--- dhakaia-jamdani/src/app/Shop/page.tsx
+++ dhakaia-jamdani/src/app/Shop/page.tsx
@@ -1,11 +1,13 @@
 
-import React from 'react'
+import React, { Suspense } from 'react'
 import ShopSection from '../components/ShopPage/ShopSection'
 
 const Shop = () => {
   return (
     <div>
+      <Suspense fallback={<div>Loading...</div>}/>
       <ShopSection/>
+      <Suspense/>
     </div>
   )
 }
